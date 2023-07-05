44758c158605c0800759a1fc4d88544698038eaa
Nikhil <nikhildixit2125@gmail.com>
2023-07-05 11:31:21 +0530

    source code

---
+++ index.js
@@ -0,0 +1,11 @@
+// const sections = document.querySelectorAll(".section");
+// sections.forEach((section) => {
+//     section.addEventListener("wheel", (e) => {
+//         e.preventDefault();
+//         const container = section.closest("main");
+//         container.scrollTo({
+//             top: container.scrollTop + e.deltaY,
+//             behavior: "smooth",
+//         });
+//     });
+// });
